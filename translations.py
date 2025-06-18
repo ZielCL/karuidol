fb77106d5cb860d846814179bf649a5f1eb36a92
ZielCL <139594291+ZielCL@users.noreply.github.com>
2025-06-18 16:15:12 -0400

    Update translations.py

--- translations.py
+++ translations.py
@@ -1,6 +1,7 @@
+# translations.py
 translations = {
     "es": {
-        "invite_link": "✨ <b>¡Invita a tus amigos!</b>\n\nComparte este enlace único. Por cada usuario que use el bot con tu link, recibirás premios especiales.\n\nTu enlace:\n<code>{link}</code>\n\n¡Usa /misreferidos para ver tu progreso!",
+        "invite_link": "✨ <b>¡Invita a tus amigos!</b>\n\nComparte este enlace único. Por cada usuario que use el bot con tu link, recibirás premios especiales.\n\nTu enlace:\n<code>{link}</code>\n\n¡Usa el botón 👀 para ver tu progreso!",
         "invite_info": "👥 <b>Invitados:</b> {count}\n\n<b>Premios por invitaciones:</b>\n{rewards}\n\n¡Comparte tu enlace con tus amigos!",
         "reward_now": "🎉 ¡Nuevo premio conseguido!\nHas ganado: <b>{prize}</b> por invitar a {count} usuarios.",
         "reward_already": "✅ {prize}",
@@ -8,9 +9,10 @@ translations = {
         "your_progress": "Premios obtenidos:",
         "button_invite": "🌟 Consigue tu enlace de invitación",
         "button_progress": "👀 Ver mi progreso",
+        "invite_error": "Ocurrió un error. Intenta de nuevo más tarde.",
     },
     "en": {
-        "invite_link": "✨ <b>Invite your friends!</b>\n\nShare this unique link. For each user who joins using your link, you’ll get special rewards.\n\nYour link:\n<code>{link}</code>\n\nUse /myinvites to see your progress!",
+        "invite_link": "✨ <b>Invite your friends!</b>\n\nShare this unique link. For each user who joins using your link, you’ll get special rewards.\n\nYour link:\n<code>{link}</code>\n\nUse the 👀 button to see your progress!",
         "invite_info": "👥 <b>Invited users:</b> {count}\n\n<b>Rewards for invites:</b>\n{rewards}\n\nShare your link with your friends!",
         "reward_now": "🎉 <b>New reward!</b>\nYou have earned: <b>{prize}</b> for inviting {count} users.",
         "reward_already": "✅ {prize}",
@@ -18,5 +20,6 @@ translations = {
         "your_progress": "Your rewards:",
         "button_invite": "🌟 Get my invite link",
         "button_progress": "👀 See my progress",
+        "invite_error": "An error occurred. Please try again later.",
     }
 }
